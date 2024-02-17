8347f9b4b624e3672fc9db6bcf8282d2d568ac21
金俊成 <tkezyo@live.com>
2024-02-17 18:13:49 +0800

    删除多余代码

--- test/Test1.UI/Program.cs
+++ test/Test1.UI/Program.cs
@@ -1,62 +1,35 @@
 ﻿using Microsoft.Extensions.Hosting;
-using ReactiveUI;
 using Serilog;
 using Serilog.Events;
 using System;
-using System.Diagnostics;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Ty;
 
-namespace Test1
+namespace Test1;
+
+class Program
 {
-    class Program
+    public static async Task Main(string[] args)
     {
-        [STAThread]
-        public static async Task Main(string[] args)
-        {
-            var configuration = new LoggerConfiguration()
+        var configuration = new LoggerConfiguration()
 #if DEBUG
-           .MinimumLevel.Debug()
+       .MinimumLevel.Debug()
 #else
-            .MinimumLevel.Information()
+        .MinimumLevel.Information()
 #endif
-           .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-           //输出到文件
-           .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
-           .Enrich.FromLogContext();
+       .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+       //输出到文件
+       .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+       .Enrich.FromLogContext();
 
-            var host = await IModule.CreateHost<Test1UIModule>(args, skipVerification: true) ?? throw new Exception();
-
-            Start(host);
-        }
-        public static void Start(IHost host)
+        var host = await IModule.CreateHost<Test1UIModule>(args, skipVerification: true) ?? throw new Exception();
+        Thread thread = new(async () =>
         {
-            //var host = Host.CreateDefaultBuilder()
-            //    .ConfigureServices((context, services) =>
-            //    {
-            //        services.AddHostedService<Worker>();
-            //    })
-            //    .UseSerilog()
-            //    .Build();
-
-            //host.Run();
-            //在STA线程上运行
-            Thread thread = new(() =>
-            {
-                using (host)
-                {
-                    host.Run();
-                }
-            });
-            thread.SetApartmentState(ApartmentState.STA);
-
-            thread.Start();
-            thread.Join();
-
-        }
+            await host.RunAsync();
+        });
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
     }
-
 }
