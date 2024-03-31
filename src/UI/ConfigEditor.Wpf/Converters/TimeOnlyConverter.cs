7b473a94efc0e2dfaa214c66d6665dd7818cdb33
金俊成 <tkezyo@live.com>
2024-03-31 22:16:04 +0800

    修改验证显示

--- src/UI/ConfigEditor.Wpf/Converters/TimeOnlyConverter.cs
+++ src/UI/ConfigEditor.Wpf/Converters/TimeOnlyConverter.cs
@@ -65,4 +65,17 @@ namespace ConfigEditor.Converters
             return value?.ToString();
         }
     }
+
+    public class ObjStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+           return value?.ToString();
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString();
+        }
+    }
 }
